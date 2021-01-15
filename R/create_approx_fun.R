d6a0d0231c5e568d3060b100a191e47eb1005c52
Collin Erickson <collinberickson@gmail.com>
2021-01-15 08:48:57 -0500

    Added doc, license

--- R/create_approx_fun.R
+++ R/create_approx_fun.R
@@ -1,19 +1,42 @@
 
 #' Title
 #'
-#' @param f
-#' @param from
-#' @param to
-#' @param N
-#' @param K
-#' @param language
-#' @param varname
-#' @param vectorize_JS
+#' @param f R function taking a single input
+#' @param from Minimum x to evaluate
+#' @param to Maximum x to evaluate
+#' @param N Number of points to evaluate function at
+#' @param K Number of knots to use when fitting the spline,
+#' or a vector of knot points.
+#' @param language Output type of the function. One of "R", "Rchar" for and R
+#' function as a string, "JS" for JavaScript,
+#' @param varname The name of the variable for the output function
+#' @param vectorize_JS If the output is a JavaScript function, should code
+#' @param pretty Should the JavaScript output have new lines and tab indentation?
+#' be added so it is vectorized?
+#' @importFrom utils capture.output
 #'
-#' @return
+#' @return A function or string
 #' @export
 #'
 #' @examples
+#'
+#' f1 <- function(x) {cos(exp(2*x^1.4))}
+#' curve(f1, lwd=10)
+#' f2 <- create_approx_fun(f1,0,1, 11, 4)
+#' curve(f2, add=T, col=2, lwd=6, lty=2)
+#' f3 <- create_approx_fun(f1,0,1, 51, 14)
+#' curve(f3, add=T, col=3, lwd=6, lty=3)
+#' # Fewer points but concentrated in right end
+#' f4 <- create_approx_fun(f1,0,1, 51, K=c(0,.3,.6,.7,.8,.85,.9,.95,.97,1))
+#' curve(f4, add=T, col=4, lwd=6, lty=4)e
+#'
+#' create_approx_fun(f1,0,1, 11, 4, language='rchar')
+#' cat(create_approx_fun(f1,0,1, 11, 4, language='Js'))
+#' cat(create_approx_fun(f1,0,1, 11, 4, language='Js', varname = "gamma"))
+#' cat(create_approx_fun(f1,0,1, 11, 4, language='Js', vectorize_JS = T))
+#' cat(create_approx_fun(f1,0,1, 11, 4, language='Js', vectorize_JS = T, varname="theta"))
+#' cat(create_approx_fun(f1,0,1, 11, 4, language='Js'))
+#' cat(create_approx_fun(f1,0,1, 11, 4, language='Js', pretty=FALSE))
 create_approx_fun <- function(f, from, to, N, K, language="R", varname="x",
                               vectorize_JS=FALSE, pretty=TRUE) {
   # if (missing(knot_x)) {
